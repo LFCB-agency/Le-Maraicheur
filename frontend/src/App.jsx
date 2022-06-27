551760a6cd594243eb085a6e09cda46f6b0a631b
anislrg <anislrg.perso@gmail.com>
2022-06-27 10:26:03 +0200

    Log/page inaxessible si non log

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
 import AdminLogin from "@pages/AdminLogin";
 import Upload from "@pages/Upload";
 
 import "./App.css";
 
 function App() {
+  const [adm, setAdm] = useState({ email: "", id: null });
+
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/admin" element={<AdminLogin />} />
+        <Route exact path="/admin" element={<AdminLogin setAdm={setAdm} />} />
         <Route path="/upload" element={<Upload />} />
+        {adm.email && adm.email === "antoine@debray.com" && (
+          <Route path="/admin/log" element={<Upload />} />
+        )}
+        <Route path="*" element={<Navigate to="/upload" />} />
       </Routes>
     </div>
   );
