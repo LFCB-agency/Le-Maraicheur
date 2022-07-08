5834183fafe5c80e9a3cfdd84e5b72bf5b8328ae
Jerem <jerem@jerem.local>
2022-07-08 10:20:17 +0200

    Modal injected and working need to work on data to send front/back

--- frontend/src/pages/Preorder.jsx
+++ frontend/src/pages/Preorder.jsx
@@ -1,3 +1,12 @@
+// import axios from "@services/axios";
+// import { useState } from "react";
+
 export default function Preorder() {
-  return <p>faire des trucs avec des states</p>;
+  //   const [register, setRegister] = useState();
+  //   const [firstname, setFirstname] = useState();
+  //   const [lastname, setLastname] = useState("");
+  //   const [payment, setPayment] = useState("");
+  //   const [email, setEmail] = useState("");
+
+  return <p>a</p>;
 }
