2da984b9bf30dcf8babefa6e03ac41495e939af1
Toupin Jean-Marie <jm.toupin@outlook.fr>
2022-07-11 11:45:41 +0200

    deskstop 100%

--- frontend/src/pages/ContactForm.jsx
+++ frontend/src/pages/ContactForm.jsx
@@ -9,6 +9,7 @@ import "@components/ContactForm.css";
 import MenuBurger from "@components/MenuBurger";
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
+
 const Contact = () => {
   return (
     <div>
@@ -54,6 +55,18 @@ const Contact = () => {
             alt="image-antoine"
           ></img>
         </div>
+      </div>
+      <div className="map-contact">
+        <iframe
+          title="test"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10618.375323424!2d1.001514!3d48.29142945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e39707873d3e91%3A0xfdbfc22c3c91e249!2sLe%20Mara%C3%AEcheur!5e0!3m2!1sfr!2sfr!4v1657180505570!5m2!1sfr!2sfr"
+          width="1000"
+          height="650"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        />
       </div>
       <Footer />
     </div>
