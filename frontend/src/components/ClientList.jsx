dc5744f00b6f48f50f2de6d49a0e880f57a7d861
Jerem <jerem@jerem.local>
2022-07-12 09:11:07 +0200

    nos produits mobile first, desktop to do

--- frontend/src/components/ClientList.jsx
+++ frontend/src/components/ClientList.jsx
@@ -45,42 +45,48 @@ export default function ClientList() {
 
   return (
     <div className="client-list-container">
-      {clientList.map((clients) => {
-        const dateFormat = moment().format("l");
-        const clientStyle =
-          clients.checkboxStatus === 0
-            ? "client-list-display"
-            : "client-list-display-done";
-        return (
-          <div className={clientStyle} key={clients.id}>
-            <ul className="client-list-num">
-              <li className="clientId">#{clients.id}</li>
-              <li className="clientFn">Nom :{clients.firstname}</li>
-              <li className="clientLn">Prenom :{clients.lastname}</li>
-              <li className="clientEmail">Email :{clients.email}</li>
-              <li className="clientPayment">
-                {" "}
-                Payment :{clients.paymentMethod}
-              </li>
-              <li className="clientDate">Date d'inscription : {dateFormat}</li>
-              <select
-                value={clients.checkboxStatus}
-                onChange={(event) => updateStatus(clients.id, event)}
-              >
-                <option value="0">A contacter</option>
-                <option value="1">Deja Contacté</option>
-              </select>
-              <button
-                className="clientDelete"
-                type="button"
-                onClick={() => deleteClient(clients.id)}
-              >
-                Delete
-              </button>
-            </ul>
-          </div>
-        );
-      })}
+      {clientList.length === 0 ? (
+        <p>nothing to display</p>
+      ) : (
+        clientList.map((clients) => {
+          const dateFormat = moment().format("l");
+          const clientStyle =
+            clients.checkboxStatus === 0
+              ? "client-list-display"
+              : "client-list-display-done";
+          return (
+            <div className={clientStyle} key={clients.id}>
+              <ul className="client-list-num">
+                <li className="clientId">#{clients.id}</li>
+                <li className="clientFn">Nom :{clients.firstname}</li>
+                <li className="clientLn">Prenom :{clients.lastname}</li>
+                <li className="clientEmail">Email :{clients.email}</li>
+                <li className="clientPayment">
+                  {" "}
+                  Payment :{clients.paymentMethod}
+                </li>
+                <li className="clientDate">
+                  Date d'inscription : {dateFormat}
+                </li>
+                <select
+                  value={clients.checkboxStatus}
+                  onChange={(event) => updateStatus(clients.id, event)}
+                >
+                  <option value="0">A contacter</option>
+                  <option value="1">Deja Contacté</option>
+                </select>
+                <button
+                  className="clientDelete"
+                  type="button"
+                  onClick={() => deleteClient(clients.id)}
+                >
+                  Delete
+                </button>
+              </ul>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
