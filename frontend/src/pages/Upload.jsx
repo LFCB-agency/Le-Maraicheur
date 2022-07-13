60795cf98c571e7499ee8b6e2c5b30ae964adc60
anislrg <anislrg.perso@gmail.com>
2022-07-13 09:30:49 +0200

    .

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -104,7 +104,7 @@ export default function Upload() {
     getImage();
     getText();
   }, [categories]);
-  console.log(text);
+  // console.log(text);
   return (
     <form className="mep" onSubmit={handleSubmit}>
       <label htmlFor="upload-picture">
