ae3d7ed0f2cf29a74ddddc44575f0295e744f5de
anislrg <anislrg.perso@gmail.com>
2022-07-25 17:06:50 +0200

    fuck upload

--- frontend/src/pages/Produit.jsx
+++ frontend/src/pages/Produit.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/self-closing-comp */
 import { Link } from "react-router-dom";
 import parse from "html-react-parser";
